55de34ccc154cb9fc3a678979750259d5ca6351d
Domeh John <jdomeh77@gmail.com>
2022-01-29 16:55:01 +0000

    done

--- Welcome/tailwind.config.js
+++ Welcome/tailwind.config.js
@@ -3,8 +3,29 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        paul: "'Supermercado One', cursive"
+        paul: ["'Supermercado One'", 'cursive'],
+        roboto: ['Roboto', 'sans-serif']
       },
+      animation: {
+        blob: 'blob 6s infinite'
+      },
+
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)"
+          },
+          "30%": {
+            transform: "translate(25px, -50px) scale(1.2)"
+          },
+          '60%': {
+            transform: 'translate(90px, 90px) scale(0.8)'
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)'
+          }
+        }
+      }
     },
   },
   plugins: [],
