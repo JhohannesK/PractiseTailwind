9441992c41ad8df83e65aca74f96d25c546afdcc
Domeh John <jdomeh77@gmail.com>
2022-01-29 07:19:31 +0000

    starter

---
+++ Welcome/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        paul: "'Supermercado One', cursive"
+      },
+    },
+  },
+  plugins: [],
+}
